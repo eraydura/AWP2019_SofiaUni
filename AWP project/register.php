c04ca44900576c47255c274ad15e8b20cc223d4b
robibin <muckenhirn.r@laposte.net>
2019-06-21 18:08:44 +0200

    Add files via upload

--- AWP project/register.php
+++ AWP project/register.php
@@ -104,12 +104,11 @@ form {
       <p><strong>Address*</strong><textarea class="form-control" rows="5" name="Adress" placeholder="Address">  </textarea></p> </div>
 
       <div class="dropdown">
-    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Gender
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Man</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Woman</a></li>
-    </ul>
+    <select class="btn btn-default dropdown-toggle" id="">
+    <option class="caret" value="">--Gender--</option>
+    <option class="caret" value="man">Man</option>
+    <option class="caret" value="woman">Woman</option>
+  </select>
   </div> <br>
 
   
