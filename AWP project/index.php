0d45f3a83eff3f59646cf2c2aba311fbd817bb2e
robibin <muckenhirn.r@laposte.net>
2019-06-21 17:57:38 +0200

    Add files via upload

---
+++ AWP project/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Home</title>
+<link rel="stylesheet" href="resources/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="resources/css/style.css" type="text/css" />
+</head>
+<body>
+
+<h1 class="text-center"> List of products </h1>
+
+<?php
+require_once("composer/vendor/autoload.php");
+require_once("dbconfig.php");
+
+if(isset($_POST["submit"])) {
+	$uploadOk = 1;
+
+	$target_dir = "uploads/";
+	$target_name = $target_dir . date("Y_m_d") . basename($_FILES["uplfile"]["name"]);
+
+	$imageFileType = $_FILES["uplfile"]["type"];
+	$fileType = pathinfo($target_name, PATHINFO_EXTENSION);
+	
+	if ($fileType != "xls" && $fileType != "xlsx" ) {
+	    echo "Only xls & xlsx files are allowed.";
+	    $uploadOk = 0;
+	}
+
+	if ($uploadOk == 0) {
+		echo "Sorry, your file was not uploaded.";
+	}
+	else {
+	    if (move_uploaded_file($_FILES["uplfile"]["tmp_name"], $target_name)) {
+	        echo "The file has been uploaded.";
+	        
+	        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
+			$reader->setReadDataOnly(true);
+			$spreadsheet = $reader->load($target_name);
+			$sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+			//var_dump($sheetData);
+
+
+
+	    } else {
+	        echo "Sorry, there was an error uploading your file.";
+	    }
+	}
+
+}
+
+?>
+
+<form action="" method="POST" enctype="multipart/form-data">
+    Browse to select a file:<br>
+    <input type="file" name="uplfile" class="btn-primary" />
+    <input type="submit" name="submit" class="btn-primary" />
+</form>
+
+
+<br><br>
+<table class="table table-hover center-block">
+ <thead class="thead-dark"> 
+  <tr class="bg-info">
+      <th>Code</th>
+      <th>Name</th>
+      <th>Type</th>
+      <th>???</th>
+      <th>Materials</th>
+      <th>Short description</th>
+    </tr>
+ </thead>   
+ <tbody>
+<?php if(isset($_POST["submit"])) {
+	foreach ($sheetData as $result) {
+	?>
+	<tr>
+	<td><?php echo $result['A']; ?></td>
+	<td><?php echo $result['B']; ?></td>
+	<td><?php echo $result['C']; ?></td>
+	<td><?php echo $result['D']; ?></td>
+	<td><?php echo $result['E']; ?></td>
+	<td><?php echo $result['F']; ?></td>
+</tr>
+<?php 
+
+$sql = "INSERT INTO `products` VALUES (".$result['A'].",".$result['B'].",".$result['C'].",".$result['D'].",".$result['E'].",".$result['F'].",".$result['G'].",".$result['H'].",".$result['I'].")";
+$query = $conn->prepare($sql);
+//print($sql);
+//$query->execute();
+}
+}
+
+
+?>
+</table>
+<?php foreach ($sheetData as $line) {
+                $valuesToInsert = "";
+                foreach ($line as $row) {
+                        $valuesToInsert .= '"' . $row . '", ';
+                }
+                //search if the last char is a comma and then remove it
+                if (substr($valuesToInsert, -2, 1) == ','){
+                  $valuesToInsert = substr($valuesToInsert, 0, -2);
+                }
+
+                $sql = "INSERT INTO products VALUES(" . $valuesToInsert . ");";
+               // $query = $dbh->prepare($sql);
+               // $query->execute();
+        }?>
+</body>
+
+</html>
--- AWP project/register.php
+++ AWP project/register.php
@@ -4,7 +4,7 @@
 <html> 
 <head> 
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="resources/css/bootstrap.min.css" type="text/css"  />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -22,7 +22,23 @@ registration {
     font-family:sans-serif,bold, Arial, Helvetica; 
     font-size:14px; 
 } 
-   
+body {
+background-image:url(paysage.jpg);
+background-repeat: no-repeat;
+background-size: cover;
+background-attachment: fixed;
+}
+
+/*span
+{
+  position:relative;
+  display:block;
+  overflow:auto;
+  position:absolute;
+  top:0;
+  right:15px;
+}
+
 div {  
     box-sizing: border-box; 
     width: 100%; 
@@ -30,52 +46,90 @@ div {
     float: left; 
     align-content: center; 
     align-items: center; 
-} 
+} */
    
 form {                                         
-    margin: 0 auto; 
-    width: 600px; 
-}</style></head> 
+    margin: 5 auto; 
+    width: 465px; 
+    border-radius: 10px;
+    background-color: rgba(95,95,95,0.4);
+    padding:20px;
+}
+
+</style>
+</head> 
    
 <body> 
-<h1 style="text-align: center"> REGISTRATION FORM </h1>           
-<form class="container" name="RegForm" action="/submit.php" onsubmit="return registration()" method="post">  
-      
-    <p>Name: <input class="form-control" type="text"  name="Name" required> </p><br>        
-    <p>Surname: <input class="form-control" type="text" name="Surname" required>  </p><br> 
-    <p>E-mail Address: <input class="form-control" pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title="Invalid email address" type="text"  name="EMail" required>  </p><br> 
-  <label>Password :
-  <input name="Password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" class="form-control" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" id="password" type="password" onkeyup='check();' required />
-</label>
-<br>
-<label>Confirm password:
-  <input type="password" class="form-control" name="confirm_password" id="confirm_password"  onkeyup='check();' required/> 
-  <span id='message'></span>
-</label>
-<br>
-   <br>
-    <p>Telephone: <input class="form-control" type="text" pattern="[0-9]{10}"  title="Ten digits code"  name="Telephone" required> </p><br>   
-     <p>Credit Card: <input pattern="^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$" class="form-control" title="Must enter valid credit card" type="text" name="Card">  </p><br>      
-     
-    <p>Address:  <textarea class="form-control" rows="5" name="Adress">  </textarea></p> 
-      <input type="checkbox" name="gender" value="female" checked> FEMALE<br>
-      <input type="checkbox" name="gender" value="male"  > MALE</p>
+   
+<form class="form-group form-control-static" name="RegForm" action="/submit.php" onsubmit="return registration()" method="post">  
+      <h1 class ="form-group" style="text-align: center;"><i> Sign Up </i></h1>  
+<div class="form-group"><hr/></div>   
 
-	
-    <p><span class="input-group-text" id="basic-addon3">https://example.com/users/
-   <input type="text" class="form-control" name="pic" pattern="(http[^\s]+(jpg|jpeg|png|tiff)\b)" ></span> </p>
+    <div class="form-group">
+        <label><strong>Name*</strong> 
+      <input class="form-control" style="" type="text"  name="Name" placeholder="Name" required>
+    </label>
+      <label><strong>Surname*</strong>
+      <input class="form-control" type="text" name="Surname" placeholder="Surname" required>  </label></div>
+
+
+    <div class="form-group">
+    <p><strong>E-mail Address*</strong>
+      <div class="input-group">
+      <span class="input-group-addon">
+      <span class="glyphicon glyphicon-envelope"></span></span>
+      <input class="form-control" pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title="Invalid email address" type="text"  name="EMail" placeholder="E-mail Address" required> </div> </p></div>
+
+
+  <div class="form-group">
+    <label>Password*</label>
+  <input name="Password" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" class="form-control" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" id="password" type="password" onkeyup='check();' placeholder="********" required /></div>
+
+
+<div class="form-group">
+  <label>Confirm password*</label>
+  <input type="password" class="form-control" name="confirm_password" id="confirm_password"  onkeyup='check();' placeholder="********" required/> </div>
+<span id="message"></span>
+
+
+    <div class="form-group">
+      <p><strong>Telephone*</strong>
+      <div class="input-group">
+      <span class="input-group-addon">
+      <span class="glyphicon glyphicon-earphone"></span></span>
+      <input class="form-control" type="text" pattern="[0-9]{10}"  title="Ten digits code"  name="Telephone" placeholder="Telephone" required> </div></p></div>
+     <!--<p>Credit Card <input pattern="^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$" class="form-control" title="Must enter valid credit card" type="text" name="Card">  </p>      -->
      
-      <p>
+    <div class="form-group">
+      <p><strong>Address*</strong><textarea class="form-control" rows="5" name="Adress" placeholder="Address">  </textarea></p> </div>
+
+      <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Gender
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Man</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Woman</a></li>
+    </ul>
+  </div> <br>
+
+  
+     <div class="input-group">
+      <span class="input-group-addon">
+      https://example.com/users/</span>
+   <input type="text" class="form-control" name="pic" pattern="(http[^\s]+(jpg|jpeg|png|tiff)\b)" ></span> </p></div>
+     
+      <div class="form-group"><hr/></div> 
+
       <p>
        <input class="btn btn-success" type="submit" value="Submit" name="Submit">      
-        <input class="btn btn-danger" type="reset" value="Reset" name="Reset">   
+        <input class="btn btn-danger" style="float:right;" type="reset" value="Reset" name="Reset">   
     </p>  
    
 </form> 
 
 <script>
-		
-	
+    
+  
 var check = function() {
   if (document.getElementById('password').value ==
     document.getElementById('confirm_password').value) {
@@ -86,7 +140,7 @@ var check = function() {
     document.getElementById('message').innerHTML = 'not matching';
   }
 }
-	
+  
    </script> 
 </body> 
 </html> 
